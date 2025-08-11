8afa0a4cf1fd47f391302a359a1b68510293fdcb
ChandeepaNimsara <chandeeparodrigo97@gmail.com>
2025-08-11 22:14:58 +0530

    Add firt for CI

---
+++ tests/example1.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+
+test('Open playwright site', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.waitForTimeout(2000);
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // Console output
+  console.log("Page loaded successfully!");
+
+  
+});
+
