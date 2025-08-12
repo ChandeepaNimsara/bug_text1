167a0e92d6c5d308a17a710d795fadb2d930da3a
ChandeepaNimsara <chandeeparodrigo97@gmail.com>
2025-08-12 16:43:20 +0530

    second

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 
 test('Open playwright site', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  await page.waitForTimeout(2000);
+  await page.waitForTimeout(4000);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
